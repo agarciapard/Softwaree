26a06f1e511cd43a124e9792ebcff6c0dcf3cf64
agarciapard <agarciapard@alumni.unav.es>
2023-04-20 10:12:46 +0200

    Add a new int

--- WEB-INF/classes/C1.java
+++ WEB-INF/classes/C1.java
@@ -3,6 +3,7 @@ class C1{
 public static String salidaHTML(String dim){
 int n=Integer.parseInt(dim);
 String out="<html>";
+int a= 0
 for (int i=1; i<=n;i++){
 out =out+ "<A HREF=S1>Crear matriz "+i+"X"+i+"</A><br>";
 }
